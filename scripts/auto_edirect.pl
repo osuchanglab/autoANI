7038c68dd3d51f54df2bf5ca53084edee28b0d29
Edward Davis <davised.dev@gmail.com>
2016-12-06 14:47:29 -0800

    Updated auto_edirect.pl to find missing assembly IDs when necessary.

--- scripts/auto_edirect.pl
+++ scripts/auto_edirect.pl
@@ -12,13 +12,21 @@ my $xtract = $edirpath . 'xtract';
 my $email = '';
 my $logging;
 my $quiet = 0;
+my $help;
 
 #Get options
 my $signal = GetOptions(
                          'log:s'    => \$logging,
                          'quiet'    => \$quiet,
-                         'email=s'  => \$email
+                         'email=s'  => \$email,
+                         'h|help'   => \$help
                        );
+
+if ($help) {
+    print STDERR "Correct usage is:\n";
+    print STDERR "$0 accessions.txt > keys.tab\n";
+    exit(0);
+}
 
 if ( defined($logging) ) {
     if ( $logging eq '' ) {
@@ -44,18 +52,18 @@ close INFILE;
 
 my %idmatch;    # master record accession for WGS; accession for nt
 my @idmatch;    # master record list for submit to NCBI
-my %accn;       # key = gi num, value = acc num
+my $accn;       # HASHREF key = gi num, value = acc num
 my ( %assem, @assem );    # key = gi num, value = assembly id
 my ( %taxon, @taxon );    # key = gi num, value = taxon id
 my (%names);              # key = taxon id, value = scientific name
-my ( %gis, @gis );        # key = acc num, value = gi num
+my $gis;        # HASHREF key = acc num, value = gi num
 my %titles;
 my %country;
 my %source;
 my %gb_name;
 my %strain;
 my %culture;
-my $history;
+my $history = 'history';
 my %counter;
 my %year;
 
@@ -79,58 +87,22 @@ my $j = 1;
 
 logger( "Submitting " . ( scalar(@idmatch) ) . " ids to epost.\n" );
 
-
-my $join = join(',',@idmatch);
-
-if ( -e 'history' ) {
-    `rm -f history`;
-}
-
-my $command = join(" ", $epost, '-format', 'acc', '-db', 'nuccore', '-id', "$join", '>', 'history');
-
-system("$command") == 0 or die "Unable to run epost command : $!";
-
-if ( $? != 0 ) {
-    logger("Problem posting accession numbers to NCBI. See output message below and check your connection and try again.\n");
-    exit(4);
-}
-
-$history = `cat history`;
-
-$counter{'epost'} = getCount($history);
+$counter{'epost'} = epost(\@idmatch,$history);
 
 logger("Successfully posted $counter{'epost'} accessions.\n");
 
 #This section will become extraneous once accession.versions are acceptable.
 
-@gis = `cat history | $efetch -format docsum | xtract -pattern DocumentSummary -element AccessionVersion Gi`;
-
-chomp(@gis);
-
-logger( scalar(@gis) . " GIs returned from efetch.\n" );
-
-if (scalar(@gis) == scalar(@idmatch)){
-    #print STDERR "Returned equal number of GIs as submitted accessions!\n";
-    logger("Matching Accessions to GI numbers.\n");
-    foreach my $line (@gis) {
-        my ($acc, $gi) = split("\t",$line);
-        $accn{$gi} = $acc;
-        $gis{$acc} = $gi;
-    }
-} else {
-    logger("GIs and accessions do not match!\n");
-    logger("Send your test dataset to davised.dev\@gmail.com for further troubleshooting.\n");
-    logger("Script will continue, but output is probably missing data.\n");
-}
+($accn, $gis) = gimatch($history,$counter{'epost'});
 
 #This section will remain mostly unchanged
 
 logger("Getting genbank metadata for organism name, country, year, etc.\n");
-my @command = ( "cat history |",
+my @command = ( "cat $history |",
                 "$efetch -format gbc -mode xml -seq_start 1 -seq_stop 2 |",
                 "$xtract -insd source organism strain isolation_source db_xref country culture_collection collection_date");
 
-$command = join(" ", @command);
+my $command = join(" ", @command);
 my @output = `$command`;
 
 logger("Parsing genbank metadata.\n");
@@ -138,7 +110,7 @@ logger("Parsing genbank metadata.\n");
 foreach my $item (@output) {
     chomp($item);
     my ($acc,$organism,$strain,$source,$xref,$country,$cc,$date) = split("\t",$item);
-    my $gi = $gis{$acc};
+    my $gi = $gis->{$acc};
     if (! $gi) {
         logger("Unable to find GI for accession $acc. Skipping.\n");
         next;
@@ -172,21 +144,55 @@ foreach my $item (@output) {
     $year{$gi} = $date;
 }
 
-logger("Getting links to assembly DB.\n");
 #This section will use accession instead of GI numbers
-@command = ("cat history |",
-            "$elink -target assembly -cmd neighbor |",
-            "$xtract -pattern LinkSet -element IdList/Id Link/Id");
-$command = join(" ", @command);
 
-@output = `$command`;
+@output = assemLinks($history);
 
-logger("Parsing links to assembly DB.\n");
+my %retry;
+
+logger("Parsing links to Assembly DB.\n");
 
 foreach my $item (@output) {
     chomp($item);
     my ($gi, $assemid) = split("\t",$item);
-    $assem{$gi} = $assemid;
+    if (!$assemid) {
+        my $acc = $accn->{$gi};
+        $retry{$idmatch{$acc}} = 1;
+    } else {
+        $assem{$gi} = $assemid;
+    }
+}
+
+my $rethis = 'retryhistory';
+
+if (%retry) {
+
+    logger("Retrying assembly ID lookup with ".scalar(keys(%retry))." accessions.\n");
+
+    my @retry = sort keys %retry;
+    $counter{'retcount'} = epost(\@retry, $rethis);
+
+    my ($retaccn,$retgis) = gimatch($rethis,$counter{'retcount'});
+
+    @output = assemLinks($rethis);
+
+    foreach my $item (@output) {
+        chomp($item);
+        my ($gi, $assemid) = split("\t",$item);
+        my $acc = $retaccn->{$gi};
+        if (!$acc) {
+            print STDERR "Unable to find accession number for GI $gi\n";
+            next;
+        }   
+        my $master = getMaster($acc);
+        my $master_gi = $gis->{$master};
+        if ($assemid eq '-') {
+            $assem{$master_gi} = '-';
+        } else {
+            $assem{$master_gi} = $assemid;
+        }
+    }
+
 }
 
 logger("Mapping and printing all data.\n");
@@ -215,7 +221,79 @@ foreach my $id (@ids) {
 }
 
 logger("Done.\n");
-`rm -f history`;
+`rm -f $history`;
+`rm -f $rethis`;
+
+sub epost {
+
+    my $ids = shift; #Array reference
+    my $fn = shift;  #Filename for history output
+
+    my $join = join(',',@{$ids});
+
+    if ( -e "$fn" ) {
+        `rm -f $fn`;
+    }
+
+    my $command = join(" ", $epost, '-format', 'acc', '-db', 'nuccore', '-id', "$join", '>', "$fn");
+
+    system("$command") == 0 or die "Unable to run epost command : $!";
+
+    if ( $? != 0 ) {
+        logger("Problem posting accession numbers to NCBI. See output message below and check your connection and try again.\n");
+        exit(4);
+    }
+
+    my $his = `cat $fn`;
+
+    my $count = getCount($his);
+
+    return $count;
+}
+
+sub gimatch {
+
+    my $his = shift;
+    my $idcount = shift;
+    my (%accn, %gis);
+
+    my @gis = `cat $his | $efetch -format docsum | xtract -pattern DocumentSummary -element AccessionVersion Gi`;
+
+    chomp(@gis);
+
+    logger( scalar(@gis) . " GIs returned from efetch.\n" );
+
+    if (scalar(@gis) == $idcount){
+        #print STDERR "Returned equal number of GIs as submitted accessions!\n";
+        logger("Matching Accessions to GI numbers.\n");
+        foreach my $line (@gis) {
+            my ($acc, $gi) = split("\t",$line);
+            $accn{$gi} = $acc;
+            $gis{$acc} = $gi;
+        }
+    } else {
+        logger("GIs and accessions do not match!\n");
+        logger("Send your test dataset to davised.dev\@gmail.com for further troubleshooting.\n");
+        logger("Script will continue, but output is probably missing data.\n");
+    }
+    
+    return (\%accn, \%gis);
+}
+
+sub assemLinks {
+    my $his = shift;
+
+    logger("Getting links to Assembly DB.\n");
+#This section will use accession instead of GI numbers
+    my @cmd = ("cat $his |",
+               "$elink -target assembly -cmd neighbor |",
+               "$xtract -pattern LinkSet -element IdList/Id Link/Id");
+    my $cmd = join(" ", @cmd);
+
+    my @out = `$cmd`;
+
+    return(@out);
+}
 
 sub getMaster {
     my $id = shift;
@@ -257,7 +335,7 @@ sub getData {
     my $culture;
     my $year;
 
-    $gi = $gis{$master};
+    $gi = $gis->{$master};
     if ( !$gi ) {
         logger(
             "Unable to find GI number for accession $master. This accession will not be included in the final analysis.\n"
